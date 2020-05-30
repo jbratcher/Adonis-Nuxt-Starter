d857d8d9db99381b3f4095c9fc2a3a1a3f87f71d
Jeremy Bratcher <jeremybratcher@gmail.com>
2020-05-30 12:01:16 -0400

    refactor: sqlite to postgres

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,13 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use("Env");
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use("Helpers");
+
+const Url = require("url-parse");
+const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 
 module.exports = {
   /*
@@ -16,7 +19,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get("DB_CONNECTION", "sqlite"),
 
   /*
   |--------------------------------------------------------------------------
@@ -30,11 +33,13 @@ module.exports = {
   |
   */
   sqlite: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get("DB_DATABASE", "development")}.sqlite`
+      ),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
   /*
@@ -48,14 +53,14 @@ module.exports = {
   |
   */
   mysql: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis"),
+    },
   },
 
   /*
@@ -69,13 +74,13 @@ module.exports = {
   |
   */
   pg: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
-  }
-}
+      host: Env.get("DB_HOST", DATABASE_URL.hostname),
+      port: Env.get("DB_PORT", DATABASE_URL.port),
+      user: Env.get("DB_USER", DATABASE_URL.username),
+      password: Env.get("DB_PASSWORD", DATABASE_URL.password),
+      database: Env.get("DB_DATABASE", DATABASE_URL.pathname.substr(1)),
+    },
+  },
+};
